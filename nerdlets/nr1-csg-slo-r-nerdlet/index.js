3c9ac8eb8e11427b43eb097c3f3a250a443b4e2c
ricegi <gil@newrelic.com>
2019-10-23 10:05:55 -0400

    Initial commit
    ++ nerdlet structure

---
+++ nerdlets/nr1-csg-slo-r-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nr1CsgSloR extends React.Component {
+    render() {
+        return <h1>Hello, nr1-csg-slo-r Nerdlet!</h1>;
+    }
+}
