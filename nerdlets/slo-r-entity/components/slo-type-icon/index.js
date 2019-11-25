2180d8e2d313178ee0eee4bed2e35f4c822144e2
DevFreddy <michael.frederickjr@gmail.com>
2019-11-25 12:48:55 -0500

    chore: satisfying the angry eslint pixies

--- nerdlets/slo-r-entity/components/slo-type-icon/index.js
+++ nerdlets/slo-r-entity/components/slo-type-icon/index.js
@@ -6,110 +6,89 @@
  */
 /** core */
 import React from 'react';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 /** nr1 */
-import { Icon } from 'nr1'
-import { Spinner } from 'nr1';
+import { Icon, Spinner } from 'nr1';
 /** local */
 /** 3rd party */
 
 /**
  * SLOTypeIcon
  */
-export default class SLOTypeIcon extends Component {  
+export default class SLOTypeIcon extends React.Component {
+  static propTypes = {
+    slo_type: PropTypes.string
+  }; // propTypes
 
-    static propTypes = {
-        slo_type: PropTypes.any
-    } //propTypes
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      slo_type: null
+    }; // state
+  } // constructor
 
-    constructor(props) {
-        super(props)
+  /** lifecycle evaluates the type property and updates the render state */
+  componentDidMount() {
+    // this should be defined in the component props
+    if (this.state.slo_type === null) {
+      this.setState({ slo_type: this.props.slo_type });
+    } // if
+    else {
+      this.setState({ slo_type: 'UNKNOWN_TYPE' });
+    } // else
+  } // componentDidMount
 
-        this.state = {
-            slo_type: null
-        } //state
-
-    } //constructor
-
-    /** lifecycle evaluates the type property and updates the render state */
-    componentWillMount() {
-
-        //this should be defined in the component props
-        if (this.state.slo_type === null) {
-
-            this.setState({slo_type: this.props.slo_type});
-        }//if
-        else {
-
-            this.setState({slo_type: "UNKNOWN_TYPE"});
-        } //else
-    } //componentWillMount
-
-    /** lifecycle given the type passed to the component properties it renders a generic icon */
-    render() {
-
-        //structure the component display - make available for dynamism
-        if (this.state.slo_type === null) {
-
-            return(
-                <div>
-                    <Spinner className="centered" size={'small'}/>
-                </div>
-            );
-        } //if
-        else{
-            if (this.state.slo_type === "error_budget") {
-                
-                return(
-
-                    <div>
-                        <Icon type={ Icon.TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__APPLICATION__S_WARNING } />
-                    </div>
-    
-                );
-            } //if
-            else if (this.state.slo_type === "availability") {
-
-                return(
-
-                    <div>
-                        <Icon type={ Icon.TYPE.DATAVIZ__DATAVIZ__LINE_CHART } />
-                    </div>
-    
-                );
-            } //else if
-            else if (this.state.slo_type === "capacity") {
-
-                return(
-
-                    <div>
-                        <Icon type={ Icon.TYPE.INTERFACE__STATE__HEALTHY } />
-                    </div>
-    
-                );
-            } //else if
-            else if (this.state.slo_type === "latency") {
-
-                return(
-
-                    <div>
-                        <Icon type={ Icon.TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__APPLICATION__S_OK } />
-                    </div>
-    
-                );
-            } //else if
-            else {
-
-                return(
-
-                    <div>
-                        <Icon type={Icon.TYPE.INTERFACE__INFO__HELP} />
-                    </div>
-    
-                );
-            } //else -> default icon
-        }//else -> state load
-    }//render
-} //SLOTypeIcon
+  /** lifecycle given the type passed to the component properties it renders a generic icon */
+  render() {
+    // structure the component display - make available for dynamism
+    if (this.state.slo_type === null) {
+      return (
+        <div>
+          <Spinner className="centered" size="small" />
+        </div>
+      );
+    } // if
+    else if (this.state.slo_type === 'error_budget') {
+      return (
+        <div>
+          <Icon
+            type={
+              Icon.TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__APPLICATION__S_WARNING
+            }
+          />
+        </div>
+      );
+    } // if
+    else if (this.state.slo_type === 'availability') {
+      return (
+        <div>
+          <Icon type={Icon.TYPE.DATAVIZ__DATAVIZ__LINE_CHART} />
+        </div>
+      );
+    } // else if
+    else if (this.state.slo_type === 'capacity') {
+      return (
+        <div>
+          <Icon type={Icon.TYPE.INTERFACE__STATE__HEALTHY} />
+        </div>
+      );
+    } // else if
+    else if (this.state.slo_type === 'latency') {
+      return (
+        <div>
+          <Icon
+            type={Icon.TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__APPLICATION__S_OK}
+          />
+        </div>
+      );
+    } // else if
+    else {
+      return (
+        <div>
+          <Icon type={Icon.TYPE.INTERFACE__INFO__HELP} />
+        </div>
+      );
+    } // else -> default icon
+  } // else -> state load
+} // render
--- nerdlets/slo-r-main/index.js
+++ nerdlets/slo-r-main/index.js
@@ -6,65 +6,51 @@
  */
 /** core */
 import React from 'react';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 /** nr1 */
-import { EntitiesByDomainTypeQuery } from 'nr1';
-import { Spinner } from 'nr1';
-import { List } from 'nr1';
-import { ListItem } from 'nr1';
+import { EntitiesByDomainTypeQuery, Spinner } from 'nr1';
 /** local */
 import SLOREstate from './components/slo-r-estate';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class SloRMain extends React.Component {
-    
-    static propTypes = {
-        nerdlet_beginTS: PropTypes.any,
-        nerdlet_endTS: PropTypes.any,
-        nerdlet_duration: PropTypes.any
-    } //propTypes
+  static propTypes = {
+    nerdlet_beginTS: PropTypes.string,
+    nerdlet_endTS: PropTypes.string,
+    nerdlet_duration: PropTypes.string
+  }; // propTypes
 
-    constructor(props) {
-        super(props)
-    } //constructor
+  constructor(props) {
+    super(props);
+  } // constructor
 
-    render() {
-
-        return(
-            <div>
-                <EntitiesByDomainTypeQuery entityDomain="APM" entityType="APPLICATION">
-                    {({ loading, error, data, fetchMore }) => {
-                        if (loading) {
-                            return <Spinner />;
-                        }
-                        if (error) {
-                            return 'Error!';
-                        }
-                        return (
-                            <SLOREstate
-                                entities_data={data}
-                                entities_fetchmoar={fetchMore}
-                                nerdlet_beginTS={this.props.nerdlet_beginTS}
-                                nerdlet_endTS={this.props.nerdlet_endTS}
-                                nerdlet_duration={this.props.nerdlet_duration}
-                            />
-                            );
-                        }
-                    }
-                </EntitiesByDomainTypeQuery>
-            </div>
-        );
-
-
-        
-
-
-
-    } //render
-
-} //SloRMain
+  render() {
+    return (
+      <div>
+        <EntitiesByDomainTypeQuery entityDomain="APM" entityType="APPLICATION">
+          {({ loading, error, data, fetchMore }) => {
+            if (loading) {
+              return <Spinner />;
+            }
+            if (error) {
+              return 'Error!';
+            }
+            return (
+              <SLOREstate
+                entities_data={data}
+                entities_fetchmoar={fetchMore}
+                nerdlet_beginTS={this.props.nerdlet_beginTS}
+                nerdlet_endTS={this.props.nerdlet_endTS}
+                nerdlet_duration={this.props.nerdlet_duration}
+              />
+            );
+          }}
+        </EntitiesByDomainTypeQuery>
+      </div>
+    );
+  } // render
+} // SloRMain
 /**
  *     {this.state.entities.map(_entity => 
                         
