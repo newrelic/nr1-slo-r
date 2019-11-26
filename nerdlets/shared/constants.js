3ec4653ac568a2840d91a6bb5847e80d3a213ffc
DevFreddy <michael.frederickjr@gmail.com>
2019-11-26 14:50:00 -0500

    feat: add constants

---
+++ nerdlets/shared/constants.js
@@ -0,0 +1,6 @@
+export const SLO_TYPES = [
+  { field: 'error_budget', name: 'Error Budget' },
+  { field: 'availability', name: 'Availability' },
+  { field: 'capacity', name: 'Capacity' },
+  { field: 'latency', name: 'Latency' }
+];
