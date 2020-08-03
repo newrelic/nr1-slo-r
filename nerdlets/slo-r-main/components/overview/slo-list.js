f74ce88fca8377287e1d3279414ae8d2846c7651
artur.wozniak <wozniaka83@gmail.com>
2020-08-03 13:56:45 +0200

    slo: slo overview with tags and tooltip

--- nerdlets/slo-r-main/components/overview/slo-list.js
+++ nerdlets/slo-r-main/components/overview/slo-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Tooltip } from 'nr1';
 import { sloIndicatorLabelLookup } from '../../../shared/helpers';
 
 const SloList = ({ slos, selectedSlosIds, handleSloClick }) => {
@@ -9,6 +10,20 @@ const SloList = ({ slos, selectedSlosIds, handleSloClick }) => {
       <ul className="slos-container__list">
         {slos.map(({ id, document }) => {
           const isSelected = selectedSlosIds.includes(id);
+
+          let tags;
+          if (document.tags) {
+            tags = document.tags.map(tag => {
+              return (
+                <Tooltip key={tag.key} text={`${tag.key}=${tag.values[0]}`}>
+                  <span className="slo__tag" key={tag.key}>
+                    {tag.values[0]}
+                  </span>
+                </Tooltip>
+              );
+            });
+          }
+
           return (
             <li
               onClick={() => handleSloClick(id, isSelected)}
@@ -21,6 +36,7 @@ const SloList = ({ slos, selectedSlosIds, handleSloClick }) => {
                   value: document.indicator
                 })}
               </span>
+              {tags ? <div className="slo__tags">{tags}</div> : null}
             </li>
           );
         })}
