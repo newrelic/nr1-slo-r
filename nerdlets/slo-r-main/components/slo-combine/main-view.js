90b417bd27fa30924baae4870e59cb1511436c98
artur.wozniak <wozniaka83@gmail.com>
2020-08-06 14:48:58 +0200

    chore: slo/r overview empty state

--- nerdlets/slo-r-main/components/slo-combine/main-view.js
+++ nerdlets/slo-r-main/components/slo-combine/main-view.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StackItem, Button } from 'nr1';
+import { StackItem, Button, UserStorageMutation, UserStorageQuery } from 'nr1';
+import { EmptyState } from '@newrelic/nr1-community';
 import { Multiselect } from 'react-widgets';
 import isEqual from 'lodash.isequal';
 
-import EmptyState from './empty-state';
 import SloList from './slo-list';
 import MainContent from './main-container';
+
+const SLO_COLLECTION_KEY = 'slo_collection_v1';
+const SLO_DOCUMENT_ID = 'slo_document';
+
+const NO_SLO_DESCRIPTION = (
+  <div>
+    It looks like no SLOs have been defined for this entity. To get started,
+    define an SLO using the button below and follow the instructions. For more
+    information please see the{' '}
+    <a
+      href="https://github.com/newrelic/nr1-slo-r"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      documentation
+    </a>
+    . We also have documentation for more specific information about{' '}
+    <a
+      href="https://github.com/newrelic/nr1-slo-r/blob/master/docs/error_slos.md"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      error driven SLOs
+    </a>{' '}
+    or{' '}
+    <a
+      href="https://github.com/newrelic/nr1-slo-r/blob/master/docs/alert_slos.md"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      alert driven SLOs
+    </a>
+  </div>
+);
 
 export default class MainView extends Component {
   constructor(props) {
@@ -16,9 +50,22 @@ export default class MainView extends Component {
       aggregatedIds: [],
       isProcessing: true,
       selectedTags: [],
-      filteredSlos: []
+      filteredSlos: [],
+      isSaving: false
     };
   }
+
+  componentDidMount = async () => {
+    const { data } = await UserStorageQuery.query({
+      collection: SLO_COLLECTION_KEY,
+      documentId: SLO_DOCUMENT_ID
+    });
+
+    this.setState({
+      aggregatedIds: data.selectedIds,
+      selectedSlosIds: data.selectedIds
+    });
+  };
 
   handleSelectSlo = (id, isSelected) => {
     this.setState(prevState => {
@@ -83,6 +130,24 @@ export default class MainView extends Component {
     }
   };
 
+  handleSave = async () => {
+    this.setState({
+      isSaving: true
+    });
+
+    await UserStorageMutation.mutate({
+      actionType: UserStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
+      collection: SLO_COLLECTION_KEY,
+      documentId: SLO_DOCUMENT_ID,
+      document: { selectedIds: this.state.selectedSlosIds }
+    });
+
+    this.setState(prevState => ({
+      aggregatedIds: prevState.selectedSlosIds,
+      isSaving: false
+    }));
+  };
+
   render() {
     const {
       isProcessing,
@@ -142,12 +207,8 @@ export default class MainView extends Component {
               <Button
                 type={Button.TYPE.PRIMARY}
                 className="button"
-                loading={false}
-                onClick={() =>
-                  this.setState({
-                    aggregatedIds: selectedSlosIds
-                  })
-                }
+                loading={this.state.isSaving}
+                onClick={this.handleSave}
               >
                 Save
               </Button>
@@ -155,11 +216,22 @@ export default class MainView extends Component {
           )}
         </StackItem>
         <StackItem grow className="main-content-container">
-          {!isProcessing && slos.length === 0 && <EmptyState />}
-          <MainContent
+          <EmptyState
+            buttonText=""
+            heading="No Slos selected"
+            description="Combine SLOs by selecting them from left sidebar."
+          />
+          <div>
+            <EmptyState
+              buttonText=""
+              heading="Get started"
+              description={NO_SLO_DESCRIPTION}
+            />
+          </div>
+          {/* <MainContent
             timeRange={timeRange}
             slos={slos.filter(slo => aggregatedIds.includes(slo.id))}
-          />
+          /> */}
         </StackItem>
       </>
     );
